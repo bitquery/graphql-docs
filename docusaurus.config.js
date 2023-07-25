819ccb2586068954f56908b4ab6d4a23fdacbf45
Andrey <gridin.andrew@gmail.com>
2023-07-25 18:37:26 +0300

    change baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: "https://docs.bitquery.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/v1",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
