d39654aa6177d42775f3431381c5664bf931ac9b
Gaurav agarwal <gauravagarwal@Gauravs-MacBook-Air.local>
2023-08-15 16:28:53 +0530

    Small changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Blockchain API Documentation (Graphql Docs)`}
+      title={`Blockchain API Documentation (V1 Graphql Docs)`}
       description="Blockchain APIs documentation for querying transactions, balances, transfers, NFTs, tokens, Dex trades, Smart contract calls, events, and much more. We support 40+ blockchains, including Bitcoin, Etheruem, Solana, Polygon, Arbitrum, Optimism, etc.">
       <HomepageHeader />
       <main>
