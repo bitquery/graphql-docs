2555fa2693146f41ebc045f0e49d2ccd323c34f5
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2026-04-02 17:09:03 +0530

    Update dependencies and configuration for Docusaurus; enhance .gitignore with additional log files
    - Updated Docusaurus and related dependencies to their latest versions.
    - Refactored code to use new import structure for Prism themes.
    - Added new log files to .gitignore to prevent unnecessary tracking.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,8 +2,9 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 require("dotenv").config();
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes: prismThemes } = require("prism-react-renderer");
+const lightCodeTheme = prismThemes.github;
+const darkCodeTheme = prismThemes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
